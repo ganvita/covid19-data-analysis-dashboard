4d0d8be78aa15c6f4e0f39c33d0413d6828e44bc
ganvita <80328249+ganvita@users.noreply.github.com>
2025-09-11 01:04:19 +0530

    Added analysis.sql (trend analysis queries)

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,13 +1,21 @@
 -- Step 2: General analysis queries
 
 -- Daily new cases & deaths per country
-SELECT location, date, new_cases, new_deaths
+SELECT 
+    country, 
+    date, 
+    new_cases, 
+    new_deaths
 FROM covid19_cleaned
-ORDER BY location, date;
+ORDER BY country, date;
 
 -- Vaccination progress as % of population
-SELECT location, date, total_vaccinations, population,
-       ROUND((total_vaccinations/population)*100,2) AS vaccination_rate
+SELECT 
+    country,
+    date,
+    total_vaccinations,
+    population,
+    ROUND((total_vaccinations * 100.0) / population, 2) AS vaccination_rate
 FROM covid19_cleaned
-WHERE total_vaccinations IS NOT NULL
-ORDER BY location, date;
+WHERE population > 0
+ORDER BY country, date;
