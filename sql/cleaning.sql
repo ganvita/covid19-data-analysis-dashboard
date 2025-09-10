7e5cc1e404a9eaf19003778edd089d33b16729df
ganvita <80328249+ganvita@users.noreply.github.com>
2025-09-11 01:02:42 +0530

    Added cleaning.sql (data cleaning script)

--- sql/cleaning.sql
+++ sql/cleaning.sql
@@ -1,16 +1,15 @@
--- Step 1: Create cleaned COVID-19 dataset
+-- Create cleaned table and handle NULLs / blanks
 CREATE TABLE covid19_cleaned AS
-SELECT 
-    location,
+SELECT
+    location AS country,
     date,
-    total_cases,
-    new_cases,
-    total_deaths,
-    new_deaths,
-    total_vaccinations,
-    new_vaccinations,
-    population
+    population,
+    CASE WHEN total_cases IS NULL OR total_cases = '' THEN 0 ELSE total_cases END AS total_cases,
+    CASE WHEN new_cases IS NULL OR new_cases = '' THEN 0 ELSE new_cases END AS new_cases,
+    CASE WHEN total_deaths IS NULL OR total_deaths = '' THEN 0 ELSE total_deaths END AS total_deaths,
+    CASE WHEN new_deaths IS NULL OR new_deaths = '' THEN 0 ELSE new_deaths END AS new_deaths,
+    CASE WHEN total_vaccinations IS NULL OR total_vaccinations = '' THEN 0 ELSE total_vaccinations END AS total_vaccinations,
+    CASE WHEN new_vaccinations IS NULL OR new_vaccinations = '' THEN 0 ELSE new_vaccinations END AS new_vaccinations
 FROM covid19_raw
 WHERE continent IS NOT NULL
-  AND total_cases IS NOT NULL
-ORDER BY location, date;
+ORDER BY country, date;
